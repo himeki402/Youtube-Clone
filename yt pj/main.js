d071b90dd4e0c7cc1226ab37b3821d135c6d34de
himeki204 <sonhz123@gmail.com>
2023-09-18 19:42:09 +0700

    commit-4

--- yt pj/main.js
+++ yt pj/main.js
@@ -1,2 +1,10 @@
-var test = document.querySelector('.avatar');
-console.log(test);
+var menuButton = document.querySelector(".menu-button");
+var sideBar = document.querySelector(".sidebar");
+var sideBarLarge = document.querySelector(".sidebar-large")
+let body = document.querySelector("body")
+
+menuButton.addEventListener("click",()=>{
+    sideBar.classList.toggle("none")
+    sideBarLarge.classList.toggle("block")
+    body.classList.toggle("change-body")
+})
